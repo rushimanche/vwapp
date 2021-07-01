d6864e982797f331949794ba3957b0b9c3b4b809
rushimanche <61168471+rushimanche@users.noreply.github.com>
2021-06-30 20:47:36 -0400

    GET SP500 TICKERS

--- main.py
+++ main.py
@@ -64,7 +64,21 @@ class VWAPCalculator:
 
         return(self.listToString(pulled_df['Symbol'].values))
 
+    def getSP500Tickers(self):
+        url = 'https://www.slickcharts.com/sp500'
+        request = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'})
+        soup = bs(request.text, "lxml")
+        stats = soup.find('table',class_='table table-hover table-borderless table-sm')
+        df =pd.read_html(str(stats))[0]
+        df['% Chg'] = df['% Chg'].str.strip('()-%')
+        df['% Chg'] = pd.to_numeric(df['% Chg'])
+        df['Chg'] = pd.to_numeric(df['Chg'])
 
+        return(self.listToString(df['Symbol'].values))
+
+
+
+    
     
 
 
